e71f0737b367ae51744b8fb948948c2b15e40f08
anuradhe Dilshan <49474661+anuradhedilshan@users.noreply.github.com>
2021-11-06 20:58:23 +0530

    Add files via upload

---
+++ Handler.js
@@ -0,0 +1,6 @@
+// var a  = "[94784369951-1636205885@g.us,94764885630-1569319369@g.us]"
+// var b = JSON.parse(a)
+// console.log(b);
+
+services = '["service1", "service2", "service3"]'
+ console.log(JSON.parse(services))
